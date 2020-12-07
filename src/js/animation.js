360e7b0c466bae0c72a37f38e0ee079341f429c2
Sasha <as007ershov@gmail.com>
2020-12-08 02:15:45 +0400

    Fix bugs

--- src/js/animation.js
+++ src/js/animation.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
     });
+
+    $("#skills").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 });
